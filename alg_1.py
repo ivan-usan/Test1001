89001cca2509cae8600f40454485f98606f4fb2f
DjoTsk <joel.tsaklidji@student.uclouvain.be>
2024-10-23 18:08:43 +0200

    Playlist rnb

--- alg_1.py
+++ alg_1.py
@@ -1,2 +1,4 @@
 def algorithm():
     pass
+
+print("Liste de musique : \n - Remyboyz (Jersey Club) \n - Telephone Man (Crimson Red) \n - Don't give it away (Chris Brown, Fridayy) \n - Yvette (Vedo, Inayah) \n - P Switch (Jersey Club) \n - Say it (Tory Lanez)")
